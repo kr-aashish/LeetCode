e5171262a63ff1ac2e475ddce00a09cdd6d84f31
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-01-20 05:55:53 +0530

    [2661] [Time Beats: 6.71%] [Memory Beats: 20.73%] - LeetPush

---
+++ 2661-First-Completely-Painted-Row-or-Column.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int numberOfRows = mat.size();
+        int numberOfColumns = mat.back().size();
+        map<int, pair<int, int>> valuesInMatrix;
+        map<int, int> rows;
+        map<int, int> columns;
+
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                valuesInMatrix[mat[i][j]] = {i, j};
+            }
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            auto coordinates = valuesInMatrix[arr[i]];
+            int row = coordinates.first;
+            int column = coordinates.second;
+
+            rows[row]++;
+            columns[column]++;
+            if (rows[row] == numberOfColumns or 
+                columns[column] == numberOfRows) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
