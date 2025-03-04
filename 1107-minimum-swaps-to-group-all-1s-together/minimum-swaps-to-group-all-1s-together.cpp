5a32ba894c968b924da182bb8853dbc93bbe9a48
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-03-04 22:40:17 +0530

    Time: 6 ms (31.64%) | Memory: 76.7 MB (6.06%) - LeetSync

---
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& data) {
+        int ones = accumulate(data.begin(), data.end(), 0);
+        int cntOne = 0, maxOne = 0;
+        deque<int> dq;
+
+        for (int i = 0; i < data.size(); i++) {
+            // Add the new element into the deque
+            dq.push_back(data[i]);
+            cntOne += data[i];
+
+            // When the deque size exceeds ones, remove the leftmost element
+            if (dq.size() > ones) {
+                cntOne -= dq.front();
+                dq.pop_front();
+            }
+            maxOne = max(maxOne, cntOne);
+        }
+        return ones - maxOne;
+    }
+};
