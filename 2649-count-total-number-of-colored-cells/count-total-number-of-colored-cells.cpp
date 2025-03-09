4a44087cc146eacdd7856c2317abdee341dc4838
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-03-09 18:29:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (63.50%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/count-total-number-of-colored-cells/description/?envType=daily-question&envId=2025-03-05
 class Solution {
 public:
     long long coloredCells(int n) { return 1 + (long long)n * (n - 1) * 2; }
