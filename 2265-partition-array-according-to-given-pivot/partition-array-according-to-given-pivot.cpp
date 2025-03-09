71c471b43571c52fc59d0f4617f213ae12eade40
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-03-09 18:28:46 +0530

    Time: 0 ms (100.00%) | Memory: 127.5 MB (96.49%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/partition-array-according-to-given-pivot/description/?envType=daily-question&envId=2025-03-03
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
