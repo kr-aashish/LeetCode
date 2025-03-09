ee518beec66dca7919eaea427640aa5fcf7d6c2d
Aashish Kumar <jmphedaashish@gmail.com>
2025-03-09 17:58:10 +0530

    Refactor: Migrate LeetPush to LeetSync

--- 9-Palindrome-Number.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        string valueAsString = to_string(x);
-        string reversedValueAsString = valueAsString;
-        reverse(reversedValueAsString.begin(), reversedValueAsString.end());
-        return valueAsString == reversedValueAsString;
-    }
-};
