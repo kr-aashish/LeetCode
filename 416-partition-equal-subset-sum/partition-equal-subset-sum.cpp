9900b9c4de75b0b05f1c2b8403e3af9d1d6aac9a
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-04-07 21:30:42 +0530

    Time: 67 ms (85.39%) | Memory: 13.4 MB (90.95%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/partition-equal-subset-sum/?envType=daily-question&envId=2025-04-07
+class Solution {
+public:
+    bool canPartition(vector<int> &nums) {
+        int totalSum = 0;
+        // find sum of all array elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+        // if totalSum is odd, it cannot be partitioned into equal sum subset
+        if (totalSum % 2 != 0) return false;
+        int subSetSum = totalSum / 2;
+        int n = nums.size();
+        vector<bool> dp(subSetSum + 1, false);
+        dp[0] = true;
+        for (int curr : nums) {
+            for (int j = subSetSum; j >= curr; j--) {
+                dp[j] = dp[j] || dp[j - curr];
+            }
+        }
+        return dp[subSetSum];
+    }
+};
