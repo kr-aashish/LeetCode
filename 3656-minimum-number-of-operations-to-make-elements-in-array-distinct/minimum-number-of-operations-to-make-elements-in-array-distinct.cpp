b56f0456b8f690e9a82c39198339d29d883370ad
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-04-15 17:08:54 +0530

    Time: 7 ms (35.76%) | Memory: 28.2 MB (45.52%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,14 +1,41 @@
-// https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/?envType=daily-question&envId=2025-04-08
+// Approach: Map and iterate
+
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        vector<bool> seen(128);
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (seen[nums[i]]) {
-                return i / 3 + 1;
-            }
-            seen[nums[i]] = true;
+        unordered_map<int, int> freq;
+        for (auto num : nums) {
+            freq[num]++;
         }
-        return 0;
+
+        int index = 0;
+        int operations = 0;
+        int numsCount = nums.size();
+        while (index < numsCount) {
+            int noDuplicates = true;
+            for (auto keyValue : freq) {
+                if (keyValue.second > 1) {
+                    noDuplicates = false;
+                }
+            }
+            if (noDuplicates) {
+                break;
+            }
+
+            if (freq[nums[index]] > 1) {
+                freq[nums[index]]--;
+            }
+            if (index + 1 < numsCount and freq[nums[index + 1]] > 1) {
+                freq[nums[index + 1]]--;
+            }
+            if (index + 2 < numsCount and freq[nums[index + 2]] > 1) {
+                freq[nums[index + 2]]--;
+            }
+
+            operations++;
+            index += 3;
+        }
+
+        return operations;
     }
 };
