9691190dd44108788ff53f7680947412d9c61ced
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-04-15 19:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (67.00%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -1,20 +1,18 @@
-// https://leetcode.com/problems/divide-array-into-equal-pairs/?envType=daily-question&envId=2025-03-17
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        // Track unpaired numbers
-        unordered_set<int> unpaired;
+        vector<int> freq(501, 0);
+        for (auto num : nums) {
+            freq[num]++;
+        }
 
-        // Add numbers to set if unseen, remove if seen
-        for (int num : nums) {
-            if (unpaired.count(num)) {
-                unpaired.erase(num);
-            } else {
-                unpaired.insert(num);
+        int freqSum = 0;
+        for (auto value : freq) {
+            if (value % 2) {
+                return false;
             }
         }
 
-        // Return true if all numbers were paired
-        return unpaired.empty();
+        return true;
     }
 };
