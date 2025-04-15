3ca1e7fe87bc4eace680c3d5463d929b5fd439ce
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-04-15 22:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (88.67%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,13 +1,11 @@
-// https://leetcode.com/problems/sum-of-all-subset-xor-totals/?envType=daily-question&envId=2025-04-05
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        int result = 0;
-        // Capture each bit that is set in any of the elements
-        for (int num : nums) {
-            result |= num;
+        int orValue = 0;
+        for (auto num : nums) {
+            orValue |= num;
         }
-        // Multiply by the number of subset XOR totals that will have each bit set
-        return result << (nums.size() - 1);
+        int sz = nums.size();
+        return (1 << (sz - 1)) * orValue;
     }
 };
