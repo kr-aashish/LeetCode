e50cc106a6400f369cb973737081dff85c21079f
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-04-16 19:33:01 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (63.16%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,51 +1,20 @@
 class Solution {
-    void generateNums(int level, string currStr, string pattern, vector<string> &nums, vector<bool> &vis) {
-        if (level == pattern.size()) {
-            nums.push_back(currStr);
-            return;
-        }
-
-        if (pattern[level] == 'I') {
-            char value = currStr.back();
-
-            for (char nextValue = value + 1; nextValue <= '9'; nextValue++) {
-                if (vis[nextValue - '0']) {
-                    continue;
-                }
-
-                vis[nextValue - '0'] = true;
-                currStr.push_back(nextValue);
-                generateNums(level + 1, currStr, pattern, nums, vis);
-                currStr.pop_back();
-                vis[nextValue - '0'] = false;
-            }
-        } else {
-            char value = currStr.back();
-
-            for (char nextValue = value - 1; nextValue >= '1'; nextValue--) {
-                if (vis[nextValue - '0']) {
-                    continue;
-                }
-
-                vis[nextValue - '0'] = true;
-                currStr.push_back(nextValue);
-                generateNums(level + 1, currStr, pattern, nums, vis);
-                currStr.pop_back();
-                vis[nextValue - '0'] = false;
-            }
-        }
-    }
-
 public:
     string smallestNumber(string pattern) {
-        vector<string> nums;
-        vector<bool> vis(10, false); 
-        for (char value = '1'; value <= '9'; value++) {
-            vis[value - '0'] = true;
-            generateNums(0, string(1, value), pattern, nums, vis);
-            vis[value - '0'] = false;
+        string result = "";
+        stack<char> digits;
+
+        for (int i = 0; i <= pattern.size(); i++) {
+            digits.push(i + 1);
+
+            if (pattern[i] == 'I' or i == pattern.size()) {
+                while (!digits.empty()) {
+                    result += ('0' + digits.top());
+                    digits.pop();
+                }
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums[0];
+
+        return result;
     }
 };
