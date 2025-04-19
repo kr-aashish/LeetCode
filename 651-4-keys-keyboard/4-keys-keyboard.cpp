819404c168b1f80dd6da16e382180ada1127c4d4
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-04-19 12:59:08 +0530

    Time: 2 ms (22.43%) | Memory: 8.6 MB (75.70%) - LeetSync

---
+++ 651-4-keys-keyboard/4-keys-keyboard.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxA(int n) {
+        vector<int> dp(n);
+        dp[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            for (int k = 0; k + 3 <= i; k++) {
+                dp[i] = max(dp[i], dp[k] * (i - k - 1));
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
