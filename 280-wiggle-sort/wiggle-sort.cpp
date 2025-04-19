48717b020dcf11ed9b379f82a3a63c0d412aa2c7
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-04-19 12:42:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (51.21%) - LeetSync

--- 280-wiggle-sort/wiggle-sort.cpp
+++ 280-wiggle-sort/wiggle-sort.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < int(nums.size()) - 1; i += 2) {
-            swap(nums[i], nums[i + 1]);
+        for (int i = 0 ; i < int(nums.size()) - 1; i++) {
+            bool isEvenIdx = i % 2 == 0;
+            bool isOddIdx = !isEvenIdx;
+            if ((isEvenIdx and nums[i] > nums[i + 1]) or
+                (isOddIdx and nums[i] < nums[i + 1])) {
+                swap(nums[i], nums[i + 1]);
+            }
         }
     }
 };
