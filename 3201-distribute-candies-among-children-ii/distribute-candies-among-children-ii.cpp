ecaab6931eb3bfd59ff24b5cd51439c81f47cbe2
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-06-01 07:07:45 +0530

    Time: 2 ms (84.12%) | Memory: 9.1 MB (18.82%) - LeetSync

---
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    int calculatePower(int value, int power) {
+        return power % 2 == 0 ? 1 : -1;
+    }
+
+    long long nCr(int n, int r) {
+        if (n < r) {
+            return 0;
+        }
+
+        if (r == 2) {
+            if (n < 2) {
+                return 0;
+            } else {
+                return 1LL * n * (n - 1) / 2;
+            }
+        }
+
+        long long value = 1;
+        for (int i = 1; i <= n; i++) {
+            value *= i;
+        }
+        for (int i = 1; i <= (n - r); i++) {
+            value /= i;
+        }
+        for (int i = 1; i <= r; i++) {
+            value /= i;
+        }
+        return value;
+    }
+
+public:
+    long long distributeCandies(int n, int limit) {
+        long long numberOfWays = 0;
+        for (int i = 0; i <= 3; i++) {
+            numberOfWays += nCr(n - (limit + 1) * i + 3 - 1, 3 - 1) * nCr(3, i) * calculatePower(-1, i);
+        }
+        return numberOfWays;
+    }
+};
