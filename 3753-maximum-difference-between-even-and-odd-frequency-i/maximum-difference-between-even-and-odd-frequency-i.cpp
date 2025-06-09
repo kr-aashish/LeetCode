5b4d48a266745a592097c36d72fb1a165500a12b
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-06-09 20:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (94.35%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -9,16 +9,13 @@ public:
         int minFreqEven = s.length();
         for (int i = 0; i < 26; i++) {
             if (freq[i] % 2 and freq[i] > maxFreqOdd) {
-                cout << string(1, 'a' + i) << " " << freq[i] << endl;
                 maxFreqOdd = freq[i];
             }
             if (freq[i] != 0 and freq[i] % 2 == 0 and freq[i] < minFreqEven) {
-                cout << string(1, 'a' + i) << " " << freq[i] << endl;
                 minFreqEven = freq[i];
             }
         }
 
-        cout << maxFreqOdd << " " << minFreqEven << endl;
         return maxFreqOdd - minFreqEven;
     }
 };
