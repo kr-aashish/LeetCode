959023fd030116bfa29b9b141a062422972d8c5e
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-06-29 14:00:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (90.95%) - LeetSync

---
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/description/?envType=weekly-question&envId=2025-06-29
+class Solution {
+  public:
+  // the smallest (first) and the largest (last) nodes
+  Node* first = NULL;
+  Node* last = NULL;
+
+  void helper(Node* node) {
+    if (node) {
+      // left
+      helper(node->left);
+
+      // node 
+      if (last) {
+        // link the previous node (last)
+        // with the current one (node)
+        last->right = node;
+        node->left = last;
+      }
+      else {
+        // keep the smallest node
+        // to close DLL later on
+        first = node;
+      }
+      last = node;
+
+      // right
+      helper(node->right);
+    }
+  }
+
+  Node* treeToDoublyList(Node* root) {
+    if (!root) return NULL;
+
+    helper(root);
+
+    // close DLL
+    last->right = first;
+    first->left = last;
+    return first;
+  }
+};
