7b06bcca83f490e5823268acc1a59cb11ed1641e
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-07-16 12:05:20 +0530

    Time: 6 ms (55.29%) | Memory: 97 MB (25.88%) - LeetSync

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i/description/?envType=daily-question&envId=2025-07-16
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int res = 0;
+        vector<vector<int>> patterns = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        for (auto& pattern : patterns) {
+            int cnt = 0;
+            for (int num : nums) {
+                if (num % 2 == pattern[cnt % 2]) {
+                    cnt++;
+                }
+            }
+            res = max(res, cnt);
+        }
+        return res;
+    }
+};
