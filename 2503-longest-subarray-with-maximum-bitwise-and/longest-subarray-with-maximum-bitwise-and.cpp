98b8a4aa5b28bc1fca779dcd3fdd0bdae7e41c45
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-07-30 09:28:42 +0530

    Time: 3 ms (42.01%) | Memory: 86 MB (90.15%) - LeetSync

---
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/?envType=daily-question&envId=2025-07-30
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxVal = 0, ans = 0, currentStreak = 0;
+
+        for (int num : nums) {
+            if (maxVal < num) {
+                maxVal = num;
+                ans = currentStreak = 0;
+            }
+
+            if (maxVal == num) {
+                currentStreak++;
+            } else {
+                currentStreak = 0;
+            }
+
+            ans = max(ans, currentStreak);
+        }
+        return ans;
+    }
+};
