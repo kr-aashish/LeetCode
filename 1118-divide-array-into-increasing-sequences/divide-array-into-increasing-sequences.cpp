796dcabb7f26b2b459c6a62da8db869ac8b8377d
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-09-29 17:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (95.24%) - LeetSync

---
+++ 1118-divide-array-into-increasing-sequences/divide-array-into-increasing-sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canDivideIntoSubsequences(vector<int>& nums, int k) {
+        int pre = nums[0], cnt = 0;
+        for (int n : nums) {
+            if (n == pre)
+                ++cnt;
+            else {
+                pre = n;
+                cnt = 1;
+            }
+            if (cnt * k > nums.size()) return false;
+        }
+        return true;
+    }
+};
