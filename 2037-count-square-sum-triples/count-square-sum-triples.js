edfe151bb3ee5735dd27f42a606c7fa22835a9c8
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2025-12-08 06:18:23 +0530

    Time: 339 ms (15.38%) | Memory: 53.7 MB (90.38%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function(n) {
+    let count = 0;
+    for (let a = 1; a <= n; a++) {
+        for (let b = 1; b <= n; b++) {
+            for (let c = 1; c <= n; c++) {
+                if (a * a + b * b == c * c) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+};
